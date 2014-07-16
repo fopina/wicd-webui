d24ff9a346ea70dee11832028911d6b0440928b5
Filipe Pina <fopina@skmobi.com>
2014-07-16 13:55:47 +0100

    network property configuration modal

--- wicd_webui.py
+++ wicd_webui.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from flask import Flask, render_template,jsonify
+from flask import Flask, render_template, jsonify, abort, request
 
 import dbus
 import dbus.service
@@ -29,7 +29,7 @@ def list():
 		result['essid'] = wireless.GetWirelessProperty(network_id, 'essid')
 
 		# check if there's key/passphrase stored (WPA1/2 and WEP only, sorry)
-		result['known'] = (wireless.GetWirelessProperty(network_id, 'key') or wireless.GetWirelessProperty(network_id, 'apsk') or wireless.GetWirelessProperty(network_id, 'apsk') or False) and True
+		result['known'] = (wireless.GetWirelessProperty(network_id, 'key') or wireless.GetWirelessProperty(network_id, 'apsk') or wireless.GetWirelessProperty(network_id, 'passphrase') or False) and True
 		results.append(result)
 
 	return jsonify(data = results)
@@ -39,12 +39,84 @@ def scan():
 	wireless.Scan(True)
 	return list()
 
-@app.route('/connect/<network_id>')
+@app.route('/connect/<int:network_id>')
 def connect(network_id):
-	network_id = int(network_id)
+	is_valid_wireless_network_id(network_id)
+
 	wireless.ConnectWireless(network_id)
 
 	return jsonify(data = wireless.GetWirelessProperty(network_id, 'essid'))
+
+@app.route('/details/<int:network_id>')
+def details(network_id):
+	is_valid_wireless_network_id(network_id)
+
+	result = {}
+	result['encryption'] = 'Off'
+	if wireless.GetWirelessProperty(network_id, 'encryption'):
+		result['encryption'] = wireless.GetWirelessProperty(network_id, 'encryption_method')
+
+	result['network_id'] = network_id
+	result['bssid'] = wireless.GetWirelessProperty(network_id, 'bssid')
+	result['channel'] = wireless.GetWirelessProperty(network_id, 'channel')
+	result['quality'] = wireless.GetWirelessProperty(network_id, 'quality')
+	result['essid'] = wireless.GetWirelessProperty(network_id, 'essid')
+	if result['essid'] == '<hidden>' : result['essid'] = ''
+
+	# check if there's key/passphrase stored (WPA1/2 and WEP only, sorry)
+	result['known'] = (wireless.GetWirelessProperty(network_id, 'key') or wireless.GetWirelessProperty(network_id, 'apsk') or wireless.GetWirelessProperty(network_id, 'passphrase') or False) and True
+
+	return jsonify(data = result)
+
+@app.route('/config', methods=['POST'])
+def config():
+	'''
+	0	wpa                 	WPA 1/2 (Hex [0-9/A-F])
+  	Req: key (Key)
+	---
+	2	wpa-psk             	WPA 1/2 (Passphrase)
+	  Req: apsk (Preshared Key)
+	---
+	5	wep-hex             	WEP (Hex [0-9/A-F])
+	  Req: key (Key)
+	---
+	6	wep-passphrase      	WEP (Passphrase)
+	  Req: passphrase (Passphrase)
+	'''
+	if not request.json:
+		abort(400)
+
+	try:
+		network_id = int(request.json.get('networkid', None))
+	except:
+		network_id = -1
+
+	is_valid_wireless_network_id(network_id)
+
+	for property, value in request.json.iteritems():
+		if property.startswith('wicd-'):
+			pp = property[5:]
+			wireless.SetWirelessProperty(network_id, pp, value)
+
+	passkey = request.json.get('passkey', None)
+
+	if passkey:
+		enctype = request.json.get('wicd-enctype', None)
+		enckey = None
+		
+		if enctype in [ 'wpa', 'wep-hex']:
+			enckey = 'key'
+		elif enctype == 'wpa-psk':
+			enckey = 'apsk'
+		elif enctype == 'wep-passphrase':
+			enckey = 'passphrase'
+
+		if enckey:
+			wireless.SetWirelessProperty(network_id, enckey, passkey)
+
+
+	result = {}
+	return jsonify(data = result)
 
 @app.route('/status')
 def status():
@@ -85,8 +157,7 @@ def current():
 def is_valid_wireless_network_id(network_id):
 	if not (network_id >= 0 \
 			and network_id < wireless.GetNumberOfNetworks()):
-		return False
-	return True
+		abort(400)
 
 # init
 
